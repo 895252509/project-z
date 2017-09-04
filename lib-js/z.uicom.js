5d59609f34781d9d32f87875da0ddc145d48f800
Zhouxiang <895252509@qq.com>
2017-09-04 17:25:18 +0800

    2

--- lib-js/z.uicom.js
+++ lib-js/z.uicom.js
@@ -14,10 +14,53 @@ zzz.uicom = (function() {
     button.prototype = new area();
     button.prototype.constructor = button;
 
+    /**
+     * 菜单对象
+     * @param {*位置 继承自 zzz.shape.rect} _obj 
+     */
     function menu(_obj) {
+        this.DIRECTION_VERTICAL = "vertical";
+        this.DIRECTION_HORIZONTAL = "horizontal";
+        area.apply(this, arguments);
+        /**
+         * 菜单的方向
+         * 1. 可以是横向的类似菜单栏
+         * 2. 可以是纵向，用于右键菜单或者菜单栏弹出菜单
+         */
+        this.direcition = this.DIRECTION_VERTICAL;
 
     }
     menu.prototype = new area();
+    menu.prototype.constructor = menu;
+
+    menu.prototype.addCom = function(_com) {
+
+        }
+        /**
+         * 添加菜单项
+         */
+    menu.prototype.addItem = function(_com) {
+
+        }
+        /**
+         * 清除所有菜单项
+         */
+    menu.prototype.clearCom = function() {
+
+        }
+        /**
+         * 移除菜单项
+         */
+    menu.prototype.removeCom = function(_com) {
+
+        }
+        /**
+         * 设置菜单位置
+         */
+    menu.prototype.setPosition = function(_rect) {
+        if (!_rect instanceof zzz.shape.rect) throw zzz.error.ParameterTypeError();
+        this.rect = _rect;
+    }
 
 
     return {
